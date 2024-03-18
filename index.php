c4cd588a7373f1db34c867e93dcb24f10d7d9c30
Aditi Das <97211930+Das-Aditi@users.noreply.github.com>
2024-03-18 15:44:52 +0530

    Add files via upload

---
+++ index.php
@@ -0,0 +1,144 @@
+<!DOCTYPE html>
+<html lang="en">
+
+<head>
+
+  <meta charset="utf-8">
+  <meta name="viewport" content="width=device-width, initial-scale=1, shrink-to-fit=no">
+  <meta name="description" content="">
+  <meta name="author" content="">
+
+  <title>Covid-19 Test Center</title>
+
+  <!-- Bootstrap core CSS -->
+  <link href="vendor/bootstrap/css/bootstrap.min.css" rel="stylesheet">
+
+  <!-- Custom styles for this template -->
+  <link href="css/scrolling-nav.css" rel="stylesheet">
+
+</head>
+
+<body id="page-top">
+
+  <!-- Navigation -->
+  <nav class="navbar navbar-expand-lg navbar-dark bg-dark fixed-top" id="mainNav">
+    <div class="container">
+      <a class="navbar-brand js-scroll-trigger" href="#page-top">Covid-19 Test Center</a>
+      <button class="navbar-toggler" type="button" data-toggle="collapse" data-target="#navbarResponsive" aria-controls="navbarResponsive" aria-expanded="false" aria-label="Toggle navigation">
+        <span class="navbar-toggler-icon"></span>
+      </button>
+      <div class="collapse navbar-collapse" id="navbarResponsive">
+        <ul class="navbar-nav ml-auto">
+          <li class="nav-item">
+            <a class="nav-link js-scroll-trigger" href="#about">About </a>
+          </li>
+          <li class="nav-item">
+            <a class="nav-link js-scroll-trigger" href="#services">Symptoms</a>
+          </li>
+          <li class="nav-item">
+            <a class="nav-link js-scroll-trigger" href="#contact">Treatment</a>
+          </li>
+  <li class="nav-item active">
+            <a class="nav-link js-scroll-trigger" href="new-user-testing.php">Testing</a>
+          </li>
+           <li class="nav-item active">
+            <a class="nav-link js-scroll-trigger" href="live-test-updates.php">Live Updates</a>
+          </li>
+
+           <li class="nav-item active">
+            <a class="nav-link js-scroll-trigger" href="login.php">Admin</a>
+          </li>
+
+
+        </ul>
+      </div>
+    </div>
+  </nav>
+
+  <header class="bg-primary text-white">
+  
+    <div class="container text-center">
+    <h1>Covid-19 Test Center</h1>
+      <p class="lead">Bengaluru, Karnataka, India</p>
+      <div style="background-image: url('https://t3.ftcdn.net/jpg/04/42/78/32/360_F_442783272_I5x4J7CktXRWB09eHYvBU5bJGOvbUJSL.jpg'); background-size: cover; background-position: center; height: 50vh;"></div>
+
+      
+    </div>
+  </header>
+
+  <section id="about" >
+    <div class="container">
+      <div class="row">
+        <div class="col-lg-5 mx-auto">
+          <h2>About </h2>
+          <br/>
+          <p class="lead"> Welcome to our COVID-19 Test Center, where your health and safety are our top priorities. Our dedicated team is here to provide efficient and accurate testing services to help protect you and your community during these challenging times. Together, we can combat the spread of COVID-19 and keep our communities safe.</p>
+        </div>
+        <div class="col-lg-7 mx-auto">
+          <img src="https://news.cgtn.com/news/2020-06-25/Studies-Wearing-masks-saves-lives-amid-COVID-19-RCcdxQoxVe/img/8704bedefaba4ee08e9d83df32fbda90/8704bedefaba4ee08e9d83df32fbda90.jpeg" style="width: 100%; height: 90%;" alt="">
+        </div>
+      </div>
+    </div>
+  </section>
+  <style>
+  #about {
+    margin-bottom: -12%; /* Adjust the value as needed */
+  }
+</style>
+
+  <section id="services" >
+  <div class="container" >
+      <div class="row">
+        <div class="col-md-6">
+          <h1>Symptoms</h1>
+          <br/>
+          <p class="lead" >Recognizing the symptoms of COVID-19 is crucial in protecting yourself and those around you. Common symptoms include fever, cough, shortness of breath, fatigue, body aches, loss of taste or smell, sore throat, congestion, nausea, or diarrhea. If you experience any of these symptoms, it's important to seek medical advice and follow local health guidelines to prevent the spread of the virus.</p>
+        </div>
+        <div class="col-md-6">
+          <img src="https://fastandup-images.s3.ap-south-1.amazonaws.com/images/single-images/1623995435.jpg" class="img-fluid" style="width: 120%; height: 70%;" alt="Image">
+        </div>
+      </div>
+    </div>
+  </section>
+  <style>
+  #services {
+    margin-bottom: -22%; /* Adjust the value as needed */
+  }
+</style>
+
+  <section id="contact" >
+    <div class="container">
+      <div class="row">
+        <div class="col-md-6">
+          <h1>Treatment</h1>
+          <br/>
+          <p class="lead" >COVID-19 treatments and medications can help you feel better if you get sick. While there's no magic cure, doctors have ways to ease symptoms and help your body fight the virus. If you feel unwell, don't hesitate to reach out to healthcare professionals for advice and support.</p>
+        </div>
+        <div class="col-md-6">
+          <img src="https://t3.ftcdn.net/jpg/04/20/70/32/360_F_420703284_zGzff5LoATFs1w2XumcKoHUiMU1OK8m9.jpg" class="img-fluid" style="width: 120%; height: 80%;" alt="Image">
+        </div>
+      </div>
+    </div>
+  </section>
+
+  <!-- Footer -->
+  <footer class="py-5 bg-dark">
+    <div class="container">
+      <p class="m-0 text-center text-white">Copyright &copy; Test center 2024</p>
+    </div>
+    <!-- /.container -->
+  </footer>
+
+  <!-- Bootstrap core JavaScript -->
+  <script src="vendor/jquery/jquery.min.js"></script>
+  <script src="vendor/bootstrap/js/bootstrap.bundle.min.js"></script>
+
+  <!-- Plugin JavaScript -->
+  <script src="vendor/jquery-easing/jquery.easing.min.js"></script>
+
+  <!-- Custom JavaScript for this theme -->
+  <script src="js/scrolling-nav.js"></script>
+
+</body>
+
+</html>
